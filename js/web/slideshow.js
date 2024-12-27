5de2922b3a74202f2df280c4c22fe70cd818b93a
Okay <1123362+otse@users.noreply.github.com>
2024-12-27 16:30:59 +0100

    Better slides

--- js/web/slideshow.js
+++ js/web/slideshow.js
@@ -2,26 +2,29 @@ import app from "../app.js";
 import { hooks } from "../lib/hooks.js";
 import popup from "./popup.js";
 const slides = [
-    ['next', 'temples1.webp', 'slide-zoom', 7, 'it is a dark age of magic...'],
-    ['next', 'king1.webp', 'slide-left', 7, 'the king has ordered its court to research necromancy while also banning magic and promoting paganism']
+    ['next', 'temples1.webp', 'slide-zoom', 8, 'it is a dark age of magic and cultural decline...'],
+    ['next', 'king1.webp', 'slide-zoom', 8, 'the king has ordered its court to research necromancy while also banning magic and promoting paganism...']
     // , ['only', 'paladins.webp', 'slide-bottom', 5, 'its paladins, healers and temples are allowed to cast spells']
+    //, ['next', 'exiles2.webp', 'slide-bottom', 5, 'commoners are exiled']
+    //, ['next', 'grimoire.webp', 'slide-left', 5, 'grimoires are destroyed']
+    // , ['next', 'siege3.webp', 'slide-bottom', 4, 'holds turn into factions']
     ,
-    ['next', 'exiles2.webp', 'slide-bottom', 5, 'commoners are exiled when protesting'],
-    ['next', 'grimoire.webp', 'slide-left', 5, 'grimoires are destroyed'],
-    ['next', 'siege3.webp', 'slide-bottom', 4, 'holds turn into factions'],
-    ['keep', '', 'slide-bottom', 4, 'the lands are thrown into civil war...'],
-    ['next', 'arguing1.webp', 'slide-left', 5, 'wise wizards call for peace'],
-    ['next', 'defeat2.webp', 'slide-zoom', 4, 'and at their insistence the king stands down'],
-    ['keep', '', 'slide-bottom', 5, 'but contained within him was a dark corruption...'],
-    ['next', 'wanderer6.webp', 'slide-fall', 5, 'he clad in rags and wandered for ages'],
-    ['keep', '', 'slide-bottom', 4, 'seeding corruption in his wake...'],
-    ['next', 'book2.webp', 'slide-left', 5, 'magic was no longer outlawed']
+    ['next', 'siege3.webp', 'slide-bottom', 8, 'counties turn into factions and civil war ravages the kingdom'],
+    ['next', 'arguing1.webp', 'slide-zoom', 5, 'a gathering of wizards hold council and call for peace'],
+    ['keep', '', 'slide-zoom', 5, 'and at their insistence the king stands down']
+    //, ['next', 'defeat2.webp', 'slide-bottom', 6, 'but contained within him was a dark corruption...']
+    ,
+    ['next', 'wanderer6.webp', 'slide-shadow-king', 5, 'to become a shadow...'],
+    ['keep', '', 'slide-bottom', 5, 'seeding corruption in his wake...'],
+    ['next', 'book2.webp', 'slide-bottom', 8, 'magic was free again but remained obscure with poor sources...']
     // , ['keep', '', 'slide-bottom', 4, 'but with its poor sources remained obscure...']
     ,
-    ['next', 'coldwar1.webp', 'slide-zoom', 9, 'without a king a cold war begins with factions clashing in politics and secretive research'],
-    ['next', 'orcs3.webp', 'slide-zoom', 9, 'orcs launch skirmishes against the weakened kingdom'],
-    ['next', 'foresee5.webp', 'slide-zoom', 5, 'one day one of the wizards realizes his mistake'],
-    ['keep', '', 'slide-bottom', 5, 'and he creates a fellowship to go after the wanderer']
+    ['next', 'coldwar1.webp', 'slide-zoom', 8, 'without a king a cold war begins with factions clashing in politics and secretive research']
+    // , ['next', 'orcs3.webp', 'slide-bottom', 9, 'orcs launch skirmishes against the weakened kingdom']
+    ,
+    ['next', 'shroom.webp', 'slide-top-to-bottom', 10, 'druids use the world shroom to channel power to the kingdom'],
+    ['next', 'foresee5.webp', 'slide-zoom', 5, 'one day one of the wizards realizes the wanderer must be found and stopped'],
+    ['keep', '', 'slide-bottom', 5, 'and summons a fellowship to go after him...']
 ];
 class slideshow {
     static instance;
@@ -45,12 +48,13 @@ class slideshow {
                 this.destroy();
             }
         });
-        this.popup.content.innerHTML = `space to skip
+        this.popup.content.innerHTML = `
+			<!--space to skip-->
 			<x-slides></x-slides>`;
         this.slides = this.popup.content.querySelector('x-slides');
         this.popup.attach();
         hooks.addListener('wcrpgStep', slideshow.step);
-        this.create_new_slide();
+        this._create_new_slide();
     }
     destroy() {
         //hooks.addListener('onmousemove', this.onmousemove);
@@ -70,7 +74,7 @@ class slideshow {
             slideshow.instance.popup.reposition();
         }
     }
-    create_new_slide() {
+    _create_new_slide() {
         const slide = slides[this.slide];
         if (slide[0] == 'keep') {
             this.currentSlide.innerHTML = slide[4];
@@ -78,29 +82,31 @@ class slideshow {
         else {
             const newDiv = document.createElement('x-slide');
             newDiv.innerHTML = slide[4];
-            newDiv.style.animationName = slide[2];
+            newDiv.style.animation = `10s ease-out 0s both ${slide[2]}`;
+            newDiv.style.animation += `, 2s ease-in-out 0s both slide-fade`;
+            newDiv.classList.add(slide[2]);
             newDiv.style.background = `url(img/slides/${slide[1]})`;
             this.currentSlide = newDiv;
             this.slides.appendChild(newDiv);
             const oldSlide = this.oldSlide;
-            if (oldSlide)
-                setTimeout(() => {
-                    oldSlide.remove();
-                }, slide[3] * 1000);
+            setTimeout(() => {
+                oldSlide?.remove();
+            }, 3000);
         }
-        this.queue_new_slide();
+        this._queue_new_slide();
     }
     new_slide() {
         if (this.slide >= slides.length - 1)
             return;
         this.slide++;
         this.oldSlide = this.currentSlide;
-        this.create_new_slide();
+        this._create_new_slide();
     }
-    queue_new_slide() {
+    _queue_new_slide() {
+        const slide = slides[this.slide];
         this.timeout = setTimeout(() => {
             this.new_slide();
-        }, slides[this.slide][3] * 1000);
+        }, slide[3] * 1000);
     }
     static async step() {
         return slideshow.instance?.step() || false;
